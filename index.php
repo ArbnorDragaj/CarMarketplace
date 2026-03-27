5d13003efec41c0c4971f3e3fbb5bbd9435b33d5
ArbnorDragaj <arbnor.dragaj@student.uni-pr.edu>
2026-03-27 22:06:15 +0100

    Test

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$message = "PHP po funksionon si duhet!";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test PHP</title>
+</head>
+<body>
+    <h1><?php echo $message; ?></h1>
+    <p>Data sot: <?php echo date("Y-m-d H:i:s"); ?></p>
+</body>
+</html>
