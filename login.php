4a32949fe4b189d62d43a98b6c9030f8b7900b4c
ArbnorDragaj <arbnor.dragaj@student.uni-pr.edu>
2026-04-11 23:05:40 +0200

    Login page

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+
+
+</head>
+
+<body>
+
+    <div class="login-box">
+        <h2>Login</h2>
+
+        <form>
+            <div class="input-box">
+                <input type="text" placeholder="Username">
+            </div>
+
+            <div class="input-box">
+                <input type="password" placeholder="Password">
+            </div>
+
+            <button class="login-btn">Login</button>
+        </form>
+    </div>
+
+</body>
+</html>
