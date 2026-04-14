5975ef27895d24f774e422ff0e03769952493b69
Orlind Bajraktari <orlind.bajraktari@student.uni-pr.edu>
2026-04-14 14:57:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,22 @@ $themeColor = isset($_COOKIE['themeColor']) ? $_COOKIE['themeColor'] : 'blue';
     <p>Shiko veturat më të fundit dhe lajmet më interesante!</p>
     <p>Shiko veturat më të fundit dhe lajmet më interesante!</p>
 </section>
+<section class="cars-preview">
+    <h2>Veturat kryesore</h2>
+    <div class="car-container">
+        <?php foreach($cars as $car): ?>
+        <div class="car-card" style="background-image: url('../assets/images/<?= strtolower(str_replace(' ','',$car->getName())) ?>.jpg');">
+            <div class="overlay">
+                <h3><?= $car->getName() ?></h3>
+                <p>Viti: <?= $car->getYear() ?></p>
+                <p>Çmimi: <?= $car->getPrice() ?>€</p>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</section>
+
+<?php include 'includes/footer.php'; ?>
 
 ?>
 
