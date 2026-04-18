7549790ccaf0e5ec75c346d963459f695a5a3b70
Orlind Bajraktari <orlind.bajraktari@student.uni-pr.edu>
2026-04-18 16:09:54 +0200

    Update index.php
    headeri

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
+<?php
+// Mund të shtosh variabla këtu për të ndryshuar titullin e faqes në mënyrë dinamike
+$site_name = "Projekti Im";
+?>
+<!DOCTYPE html>
+<html lang="sq">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $page_title ?? 'Faqja Kryesore'; ?></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-<?<php>
-echo "Hello World!"
-</php>
-
+<header class="main-header">
+    <div class="container">
+        <div class="logo">
+            <h1><a href="index.php"><?php echo $site_name; ?></a></h1>
+        </div>
+        <nav class="navbar">
+            <ul>
+                <li><a href="index.php">Ballina</a></li>
+                <li><a href="rreth-nesh.php">Rreth Nesh</a></li>
+                <li><a href="sherbimet.php">Shërbimet</a></li>
+                <li><a href="kontakti.php">Kontakti</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
