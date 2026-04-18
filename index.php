c288021b314bb5dd5e77126370847d32a94f3209
Orlind Bajraktari <orlind.bajraktari@student.uni-pr.edu>
2026-04-18 16:12:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $site_name = "Projekti Im";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title ?? 'Faqja Kryesore'; ?></title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
 
