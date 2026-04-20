03a1840c98e695d8bb1071621b9743690d3fa19d
Artin-mehana1 <artin.mehana@student.uni-pr.edu>
2026-04-20 23:10:00 +0200

    //

--- contact.php
+++ contact.php
@@ -1,10 +1,26 @@
 <?php
 
+$error = "";
+$success = "";
 
+if(isset($_POST['submit'])){
 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
+if(empty($name) || empty($email) || empty($phone) || empty($message)){
+$error = "Ju lutem plotësoni të gjitha fushat!";
+}
 
+elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+$error = "Email i pavlefshëm!";
+}
 
+else{
+$success = "Mesazhi u dërgua me sukses!";
+}
 
-
+}
 ?>
