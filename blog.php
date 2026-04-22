b51d66758d511ddd86080c9b7357b27707655d41
ermal19-BS7 <ermal.berisha6@student.uni-pr.edu>
2026-04-22 18:47:28 +0200

    Update blog.php

--- blog.php
+++ blog.php
@@ -112,3 +112,14 @@ if (isset($_POST['edit_post']) && $_SESSION['role'] === 'admin') {
     exit();
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
