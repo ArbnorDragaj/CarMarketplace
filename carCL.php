52553d13eeeffa0d08c15cfd8f734b09f54ba09f
Gertiparduzi <gerti.parduzi@student.uni-pr.edu>
2026-04-23 20:29:57 +0200

    Shtimi i metodave get/set dhe funksioneve ndihmëse në klasën Car

--- carCL.php
+++ carCL.php
@@ -19,6 +19,45 @@ class Car {
         $this->price = $price;
         $this->image = $image;
     }
+
+    
+    public function getBrand() {
+        return $this->brand;
+    }
+
+    public function getModel() {
+        return $this->model;
+    }
+
+    public function getFuel() {
+        return $this->fuel;
+    }
+
+    public function getBody() {
+        return $this->body;
+    }
+
+    public function getYear() {
+        return $this->year;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function getImage() {
+        return $this->image;
+    }
+
+
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+
+
+    public function getFullName() {
+        return $this->brand . " " . $this->model;
+    }
 }
 
 ?>
