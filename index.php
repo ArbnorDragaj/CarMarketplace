3e18fc4cd5ff3b7ee5d366a0242e2bcdb6fb8e05
Orlind Bajraktari <orlind.bajraktari@student.uni-pr.edu>
2026-04-24 18:42:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $site_name = "CarMarketPlace";
                 <li><a href="rreth-nesh.php">Rreth Nesh</a></li>
                 <li><a href="sherbimet.php">Shërbimet</a></li>
                 <li><a href="kontakti.php">Kontakti</a></li>
-                <li><a href="kontakti.php" class="blog">Blog</a></li>
+                <li><a href="blog.php" class="blog">Blog</a></li>
             </ul>
         </nav>
     </div>
