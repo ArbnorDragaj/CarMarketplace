e12385c328e0b011aa52bac18744a3825da4233f
Artin-mehana1 <artin.mehana@student.uni-pr.edu>
2026-04-25 22:50:37 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -10,3 +10,23 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $email = htmlspecialchars(trim($_POST['email']));
     $phone = htmlspecialchars(trim($_POST['phone']));
     $message = htmlspecialchars(trim($_POST['message']));
+
+    
+    // Validimi
+    if(empty($name) || empty($email) || empty($phone) || empty($message)){
+        $error = "Ju lutem plotësoni të gjitha fushat!";
+    }
+    elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $error = "Email i pavlefshëm!";
+    } 
+    elseif(!preg_match("/^\+355\d{8,9}$/",$phone)){
+        $error = "Numër telefoni i pavlefshëm! (Shembull: +355XXXXXXXXX)";
+    } 
+    else {
+        $success = "Mesazhi u dërgua me sukses!";
+        
+        // Pas suksesit i zbrazim fushat
+        $name = $email = $phone = $message = "";
+    }
+}
+?>
