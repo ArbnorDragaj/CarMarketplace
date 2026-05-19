3a6e45fc3e98321840c9376002f371be3d284eac
ermal19-BS7 <ermal.berisha6@student.uni-pr.edu>
2026-05-19 09:53:35 +0200

    Lidhja me databaz

---
+++ config/db.php
@@ -0,0 +1,23 @@
+<?php
+// config/db.php
+// Lidhja me databazen MySQL duke perdorur PDO.
+
+$host = "localhost";
+$dbname = "car_marketplace";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ]
+    );
+} catch (PDOException $e) {
+    die("Database connection failed. Please check config/db.php");
+}
