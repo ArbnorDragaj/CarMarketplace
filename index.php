0eb36e8d20311df7522b087c03dbb68d6ea55fff
ArbnorDragaj <arbnor.dragaj@student.uni-pr.edu>
2026-05-25 20:04:01 +0200

    update index

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title ?? 'CarMarketPlace'; ?></title>
-    <link rel="stylesheet" href="Style/style.css">
+    <link rel="stylesheet" href="Style/style.css?v=3">
     <link rel="stylesheet" href="Style/index.css?v=2">
 </head>
 <body>
